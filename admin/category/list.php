b638cdcce04443d49da8e15f1cb28e366286e8c6
unknown <thinh1420003@gmail.com>
2022-11-14 22:49:48 +0700

    layout admin

---
+++ admin/category/list.php
@@ -0,0 +1,43 @@
+<div class="col-xl-10">
+                <hr>
+                <h2 class="text-center">
+                    Danh sách loại hàng hóa
+                </h2>
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Mã loại</th>
+                        <th scope="col">Tên loại</th>
+                        <th scope="col"></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row"><input type="checkbox" name="" id=""></th>
+                        <td>001</td>
+                        <td>Otto</td>
+                        <td><input type="button" value="Sửa" class="btn btn-primary"> <input type="button" value="Xóa" class="btn btn-primary bg-danger"></td>
+                      </tr>
+                      <tr>
+                        <th scope="row"><input type="checkbox" name="" id=""></th>
+                        <td>001</td>
+                        <td>Otto</td>
+                        <td><input type="button" value="Sửa" class="btn btn-primary"> <input type="button" value="Xóa" class="btn btn-primary bg-danger"></td>
+                      </tr>
+                      <tr>
+                        <th scope="row"><input type="checkbox" name="" id=""></th>
+                        <td>001</td>
+                        <td>Otto</td>
+                        <td><input type="button" value="Sửa" class="btn btn-primary"> <input type="button" value="Xóa" class="btn btn-primary bg-danger"></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <button type="submit" class="btn btn-primary">Chọn tất cả</button>
+                  <button type="reset" class="btn btn-primary">Bỏ chọn tất cả</button>
+                  <button type="reset" class="btn btn-primary">Xóa các mục đã chọn</button>
+                  <a href="index.php?act=adddm"><button type="button"  class="btn btn-primary" >Thêm mới</button></a> 
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
